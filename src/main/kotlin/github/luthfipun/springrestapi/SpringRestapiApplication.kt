a220e1153a771f7c7677709a3c4eb3c2767b2839
Luthfi Abdul Azis <luthfieabdulazis@gmail.com>
2022-09-06 18:52:52 +0700

    init

---
+++ src/main/kotlin/github/luthfipun/springrestapi/SpringRestapiApplication.kt
@@ -0,0 +1,11 @@
+package github.luthfipun.springrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringRestapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringRestapiApplication>(*args)
+}
---
+++ src/test/kotlin/github/luthfipun/springrestapi/SpringRestapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package github.luthfipun.springrestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringRestapiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
